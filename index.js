5681d5b0f2e59aacf203cf472590369916e530e2
Seth Lakowske <lakowske@gmail.com>
2015-04-21 13:15:45 -0500

    add unit test

--- index.js
+++ index.js
@@ -3,23 +3,119 @@
  */
 
 var pgReqLogger = require('pg-http-request-logger');
+var uuid        = require('node-uuid');
 
-function classifyTable(client, callback) {
+function classTable(client, callback) {
 
     //Depends on requests table existing
     pgReqLogger.requestTable(client, function(err, result) {
         var createClasses = 'create table if not exists classes ('
-            + 'class_id uuid primary key default uuid_generate_v4(),'
+            + 'class_id text primary key,'
             + 'class_time timestamptz default current_timestamp,'
-            + 'request_id uuid references requests(request_id),'
-            + 'user text not null,'
+            + 'request_id text references requests(request_id) not null,'
+            + 'user_id text not null,'
             + 'class text not null'
             + ')'
+
+        console.log(createClasses);
 
         client.query(createClasses, callback);
     })
 
-    
-})
-    
-    
+}
+
+/*
+ * Insert a classification into the database.
+ *
+ * clazz {Object} - an object containing a request_id, user_id and clazz
+ */
+function insertClass(client, clazz, callback) {
+
+    var insertClass = 'insert into classes (class_id, request_id, user_id, class) VALUES ($1, $2, $3, $4)';
+
+    var id = clazz.class_id;
+    if (id === undefined) id = uuid.v4();
+
+    client.query(insertClass,
+                 [id,
+                  clazz.request_id,
+                  clazz.user_id,
+                  clazz.clazz],
+                 function (err, result) {
+                     callback(err, result, id);
+                 }
+                );
+
+}
+
+/*
+ * Get the user's next unclassified request
+ *
+ * user {String} - a string containing a user_id
+ */
+function nextRequest(client, user, count, callback) {
+
+    var nextRequest = "select r.* from requests r where r.request_id not in (select request_id from classes where user_id=$1 and request_id is not null) limit $2";
+
+    client.query(nextRequest,
+                 [user,
+                 count],
+                 callback);
+}
+
+function dropClassTable(client, callback) {
+
+    var dropClasses = 'drop table classes cascade';
+
+    client.query(dropClasses, callback);
+
+}
+
+/*
+ * Chain together a number of database operations
+ */
+function chain(client, fns, index) {
+    if (index === fns.length) return;
+
+    var client = arguments[0];
+    var fns    = arguments[1];
+    var index  = arguments[2];
+
+
+    var callback = function(err, result) {
+        var extras = []
+        extras.push(client);
+        extras.push(fns);
+        extras.push(index+1);
+
+        for (var j = 2 ; j < arguments.length ; j++) {
+            extras.push(arguments[j]);
+        }
+
+        extras.push(err);
+        extras.push(result);
+
+        if (err) {
+            console.log(err);
+        } else {
+            chain.apply(this, extras)
+        }
+    }
+
+    var varArgs = [];
+    varArgs.push(client);
+    varArgs.push(callback);
+
+    for (var j = 3 ; j < arguments.length ; j++) {
+        varArgs.push(arguments[j]);
+    }
+
+    //console.log(index);
+    fns[index].apply(this, varArgs);
+}
+
+module.exports.classTable     = classTable;
+module.exports.insertClass    = insertClass;
+module.exports.nextRequest    = nextRequest;
+module.exports.dropClassTable = dropClassTable;
+module.exports.chain          = chain;
---
+++ test.js
@@ -0,0 +1,102 @@
+/*
+ * (C) 2015 Seth Lakowske
+ */
+
+var pg            = require('pg');
+var pgReqClassify = require('./');
+var pgReqLogger   = require('pg-http-request-logger');
+var test          = require('tape');
+
+function connectOrFail(t, callback) {
+
+    var user = process.env['USER'];
+    var connection = 'postgres://'+user+'@localhost/request';
+
+    var client = new pg.Client(connection);
+
+    client.connect(function(err) {
+
+        if (err) {
+            return console.error('error fetching client from pool', err);
+            t.ok(false, "client connection failed to " + connection);
+        }
+
+        callback(client);
+
+    })
+
+}
+
+test('can create the classes table', function(t) {
+
+    connectOrFail(t, function(client) {
+        pgReqClassify.chain(client, [pgReqLogger.requestTable, pgReqClassify.classTable, pgReqClassify.dropClassTable, pgReqLogger.dropRequestTable, finish(t)], 0);
+    })
+
+})
+
+test('can classify request', function(t) {
+    var req1 = function(client, callback) {
+        pgReqLogger.insertRequest(client, {host:'sethlakowske.com', cookie:'yum', remoteAddress:'10.0.0.1',
+                                           method:'GET', url:'/', 'user-agent':'secret agent man'}, callback);
+    }
+
+    var class1 = function(client, callback, id) {
+        pgReqClassify.insertClass(client, { request_id:id, user_id:'robotron', clazz:'PrimaryReq'}, callback)
+    }
+
+    var finish = function(client, callback, id, err, result) {
+        t.ok(id, 'verify id');
+        if (err) {
+            console.log(err);
+        }
+        client.end();
+        t.end();
+    }
+    connectOrFail(t, function(client) {
+        pgReqClassify.chain(client, [pgReqClassify.classTable, req1, class1, finish], 0);
+    })
+})
+
+function finish(t, rows)  {
+    return function(client, callback, err, result) {
+        if (err) {
+            console.log(err);
+        }
+        t.ok(result, 'verify result');
+        if (rows) {
+            t.equal(result.rowCount, rows);
+        }
+        client.end();
+        t.end();
+    }
+}
+
+test('can query for unclassified requests', function(t) {
+
+    var req1 = function(client, callback) {
+        pgReqLogger.insertRequest(client, {host:'sethlakowske.com', cookie:'yum', remoteAddress:'10.0.0.1',
+                                           method:'GET', url:'/req1', 'user-agent':'secret agent man'}, callback);
+    }
+
+    var req2 = function(client, callback) {
+        pgReqLogger.insertRequest(client, {host:'sethlakowske.com', cookie:'yum', remoteAddress:'10.0.0.1',
+                                           method:'GET', url:'/req2', 'user-agent':'secret agent man'}, callback);
+    }
+
+    var class1 = function(client, callback, id) {
+        pgReqClassify.insertClass(client, { request_id:id, user_id:'robotron', clazz:'Primary'}, callback)
+    }
+
+    var class2 = function(client, callback, id) {
+        pgReqClassify.insertClass(client, { request_id:id, user_id:'seth', clazz:'Primary'}, callback)
+    }
+
+    var query = function(client, callback) {
+        pgReqClassify.nextRequest(client, 'seth', 2, callback);
+    }
+
+    connectOrFail(t, function(client) {
+        pgReqClassify.chain(client, [pgReqLogger.requestTable, pgReqClassify.classTable, req1, class1, req2, class2, query, finish(t, 2)], 0);
+    })
+})
